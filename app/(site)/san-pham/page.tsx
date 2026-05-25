a50f9759775402771f5cf6460fa0fd4548a95f3c
hnilney <lhung0099@gmail.com>
2026-05-25 20:29:17 +0700

    create page san-pham

---
+++ app/(site)/san-pham/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ProductPage() {
+    return (
+        <div>
+            <h1>Product Page</h1>
+            {/* Add your product listing or details here */}
+        </div>
+    );
+}
